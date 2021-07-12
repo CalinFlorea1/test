2579c0cbed374d13dfc59c39a39c2ca4a2a5471d
Calin <calinflorea.a@gmail.com>
2021-07-12 09:21:55 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,53 @@
+import java.util.*
+
+fun readList () : MutableList <Int>{
+    val list : MutableList <Int> = mutableListOf<Int>()
+    val reader = Scanner(System.`in`)
+
+    val n = reader.nextInt()
+
+    for (i in 1..n){
+       list.add (reader.nextInt())
+    }
+
+    return list
+}
+
+fun produsCartezian (list1 : MutableList <Int>, list2 : MutableList <Int>) : MutableList <Pair <Int, Int>>{
+    val list : MutableList <Pair <Int, Int> > = mutableListOf()
+    for (i in list1)
+        for (j in list2)
+        {
+            list.add (Pair(i, j))
+        }
+    return list
+}
+
+fun intersection (list1 : MutableList <Pair <Int, Int> >, list2 : MutableList <Pair <Int, Int> >) : MutableList <Pair <Int, Int> >{
+    val list : MutableList <Pair <Int, Int> > = mutableListOf()
+    for (i in list1)
+        for (j in list2)
+        {
+            if (i.first == j.first && i.second == j.second){
+                list.add (i)
+            }
+        }
+
+    val listFinal : MutableList <Pair <Int, Int> > = mutableListOf()
+    for (i in 1..list.size) {
+        var control = 0
+        for (j in (i + 1)..list.size) {
+            if (list[i].first == list[j].first && list[i].second == list[j].second) {
+                control = 1
+            }
+        }
+        if (control == 0){
+            listFinal.add (list[i])
+        }
+    }
+    return listFinal
+}
+
+fun main (Args : Array <String>){
+
+}
